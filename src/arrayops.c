af7d59f59b36398f9ea544f7f9dc12ac7db9bd3a
Kostis Dimos <kostisd83@gmail.com>
2020-11-27 21:01:29 +0200

    v. 0.1: integer arrays: sum, mean, max

---
+++ src/arrayops.c
@@ -0,0 +1,61 @@
+/* Copyright (c) 2020 Kostis Dimos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+int arraySum(int arr[], int arr_len)
+{
+   int i, sum=0;
+   for (i=0; i<arr_len; i++)
+   {
+	    sum = sum + arr[i];
+   }
+   return(sum);
+}
+
+double arrayMean(int arr[], int arr_len)
+{
+   int i, sum=0;
+   for (i=0; i<arr_len; i++)
+   {
+	    sum = sum + arr[i];
+   }
+   return (((double) sum / (double) arr_len));
+}
+
+int arrayMax(int arr[], int arr_len)
+{
+   int i;
+   int max = arr[0];
+   for (i=1; i<arr_len; i++)
+   {
+     if (arr[i] > max)
+        max = arr[i];
+   }
+   return max;
+}
+
+void arrayPrint(int arr[], int arr_len)
+{
+   int i;
+   for(i=0; i<arr_len; i++)
+   {
+	    printf("%d ", arr[i]);
+   }
+   printf("\n");
+}
